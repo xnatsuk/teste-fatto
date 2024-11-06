7c4f797c94f2c1c85e51e8916c16318ae2eac6de
xnatsuk <xnatsuk@gmail.com>
2024-11-06 01:21:18 -0300

    chore: update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,11 +7,14 @@ export default defineNuxtConfig({
     '@nuxt/icon',
   ],
 
+  ssr: false,
+
   devtools: { enabled: true },
 
   compatibilityDate: '2024-04-03',
 
   nitro: {
+    preset: 'cloudflare_pages',
     esbuild: {
       options: {
         target: 'esnext',
